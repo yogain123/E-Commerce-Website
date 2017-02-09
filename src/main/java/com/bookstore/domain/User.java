aa96c465180c0272ece860d3d9059c4757a3ec48
Saxena <Yogendra.Saxena@unisys.com>
2017-02-09 20:35:22 +0530

    Added User Model

---
+++ src/main/java/com/bookstore/domain/User.java
@@ -0,0 +1,15 @@
+package com.bookstore.domain;
+
+public class User {
+	
+	private long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String phone;
+	private boolean enable=true;
+
+}
